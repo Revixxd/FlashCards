6906d5ef876a85844847d860b0defbd3715da7d7
Adam Myślicki <amyslicki2002@gmail.com>
2024-12-04 19:58:01 +0100

    [SCRUM-20] setup basic routes and basic file schema

---
+++ app-front/src/modules/FlashCardsUi/Navigation/Navigation.vue
@@ -0,0 +1,15 @@
+<template>
+    <nav>
+        <RouterLink v-for="(route, key) in routes" :to="route.path" :key="key"> {{ route.name }}</RouterLink>
+    </nav>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+import { routes } from '@src/routes';
+
+</script>
+
+<style scoped>
+
+</style>
--- app-front/vite.config.ts
+++ app-front/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@FlashCardsUi': path.resolve(__dirname, './src/modules/FlashCardsUi'),
+      '@src': path.resolve(__dirname, './src'),
+    },
+  },
 })
