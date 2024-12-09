009a51cfcf98650f9641cfc315e281f5417b33f1
Adam Myślicki <amyslicki2002@gmail.com>
2024-12-09 17:44:00 +0100

    [Scrum-22] adding conditional to check if user Is Authenticated

--- app-front/src/routes.ts
+++ app-front/src/routes.ts
@@ -1,8 +1,8 @@
 import { createMemoryHistory, createRouter } from 'vue-router'
-
 import HomeView from './views/main/HomeView/HomeView.vue'
 import LoginView from './views/main/LoginView/LoginView.vue'
 import DashBoardView from './views/user/DashboardView/DashBoardView.vue'
+import isUserAuthenticated from './utils/checkUser/isUserAuthenticated'
 
 export const routes = [
   {
@@ -14,6 +14,9 @@ export const routes = [
     path: '/dashboard',
     component: DashBoardView,
     name: 'dashboard',
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
     path: '/login',
@@ -26,3 +29,15 @@ export const router = createRouter({
   history: createMemoryHistory(),
   routes,
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth) {
+    if (!isUserAuthenticated()) {
+      next();
+    } else {
+      next('/login');
+    }
+  } else {
+    next();
+  }
+});
