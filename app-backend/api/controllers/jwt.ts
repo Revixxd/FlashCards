e6a5123b30f167231bdfbdee77560b9548970c2b
Dominik Obuch <dominik.obuch@wp.pl>
2024-12-15 16:42:21 +0100

    Added jwt token to verify user added jwt implementation moved hashing password to controllers folder changed todo file to update progress

---
+++ app-backend/api/controllers/jwt.ts
@@ -0,0 +1,32 @@
+import jwt from "jsonwebtoken";
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const createJwtToken = (payload: object): Promise<string> => {
+    return new Promise((resolve, reject) => {
+        const { JWT_PRIVATE_KEY } = process.env;
+        if (!JWT_PRIVATE_KEY) {
+            throw new Error("JWT_PRIVATE_KEY is not defined in environment variables");
+        }
+        jwt.sign({ payload }, JWT_PRIVATE_KEY, { expiresIn: "12h" }, (err, token) => {
+            if (err) {
+                reject(err);
+            }
+            resolve(token as string)
+        })
+    })
+}
+export const verifyJwtToken = (token: string): Promise<object> => {
+    return new Promise((resolve, reject) => {
+        const { JWT_PRIVATE_KEY } = process.env;
+        if (!JWT_PRIVATE_KEY) {
+            throw new Error("JWT_PRIVATE_KEY is not defined in environment variables");
+        }
+        jwt.verify(token, JWT_PRIVATE_KEY, (err, decoded ) => {
+            if (err) {
+                reject(err);
+            }
+            resolve(decoded as object);
+        })
+    })
+}
