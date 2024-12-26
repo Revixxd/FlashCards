a5bd4eeff2e55e934839ac7e4174bea9debf93cf
Dominik Obuch <dominik.obuch@wp.pl>
2024-12-26 22:44:10 +0100

    [SCRUM-26] Added refresh token and implemented it for login route

--- app-backend/api/models/User.ts
+++ app-backend/api/models/User.ts
@@ -4,6 +4,7 @@ interface IUser extends Document {
     username: string;
     email: string;
     password: string;
+    refreshToken: string;
     createdAt: Date;
 }
 
@@ -11,6 +12,7 @@ const UserSchema: Schema = new Schema({
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    refreshToken: { type: String, required: false },
     createdAt: { type: Date, default: Date.now }
 });
 
