4ed74272659c6de983531ce4ca9c599886305858
Dominik Obuch <dominik.obuch@wp.pl>
2025-01-10 16:03:19 +0100

    [SCRUM-24] Created authorizeUser function to remove authorization inside routes

--- app-backend/api/controllers/auth.ts
+++ app-backend/api/controllers/auth.ts
@@ -1,4 +1,8 @@
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import sanitizeInput from '../utils/sanitizeInput.js';
+import { convertAccessToken, convertRefreshToken, createAccessToken } from './jwt.js';
+import { Request, Response } from 'express';
 
 const saltRounds = 10;
 
@@ -12,3 +16,48 @@ export const checkPassword = async (password: string, hashedPassword: string): P
     const match = await bcrypt.compare(password, hashedPassword);
     return match;
 };
+
+export const authorizeUser = async (req: Request, res: Response): Promise<string | void> => {
+    let userId: any;
+    let accessToken = (req.headers.authorization?.split(" ")[1]);
+    const refreshToken = sanitizeInput(req.cookies?.refreshToken);
+    try {
+        if (accessToken) {
+
+            userId = await convertAccessToken(accessToken);
+            const newAccessToken = await createAccessToken({ user: userId });
+            res.header("Authorization", `Bearer ${newAccessToken}`);
+            return userId;
+        }
+        else if (!accessToken && refreshToken) {
+            userId = await convertRefreshToken(refreshToken);
+            const newAccessToken = await createAccessToken({ user: userId });
+            res.header("Authorization", `Bearer ${newAccessToken}`);
+            return userId;
+
+        }
+        else {
+            res.status(401).json({ message: "Invalid access token" });
+            return;
+        }
+
+    } catch (error) {
+        if (error instanceof jwt.TokenExpiredError) {
+            try {
+                userId = await convertRefreshToken(refreshToken);
+                const newAccessToken = await createAccessToken({ user: userId });
+                res.header("Authorization", `Bearer ${newAccessToken}`);
+                return userId;
+            } catch (refreshError) {
+                res.status(401).json({ message: "Invalid refresh token" });
+                return userId;
+            }
+        }
+        else{
+            console.error("Error authorizing user:", error);
+            res.status(500).json({ message: "Internal server error" });
+            return;
+        }
+
+    }
+}
