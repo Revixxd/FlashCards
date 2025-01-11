ab09d5bc2b42fc7d12d8fbfc99ef4205d5718899
Dominik Obuch <dominik.obuch@wp.pl>
2025-01-11 14:53:02 +0100

    [SCRUM-24] Small fixes and deleted unused file

--- app-backend/api/utils/sanitizeInput.ts
+++ app-backend/api/utils/sanitizeInput.ts
@@ -1,8 +1,8 @@
 import validator from 'validator';
 
 const sanitizeInput = (input: string | undefined): string => {
-    if(typeof(input) === 'undefined' || input === null){
-        console.error("input is null or undefined");
+    if(typeof(input) === 'undefined' || input === null || input === '') {
+        console.warn("input is null or undefined");
         return '';
     }
     return validator.escape(input);
