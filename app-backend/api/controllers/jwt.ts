b71eadbe1838ca3d9b659338bd21f1aafc51e6d8
Dominik Obuch <dominik.obuch@wp.pl>
2025-01-11 14:43:24 +0100

    [SCRUM-24] Added authorization function and function converting refresh token

--- app-backend/api/controllers/jwt.ts
+++ app-backend/api/controllers/jwt.ts
@@ -2,13 +2,13 @@ import jwt from "jsonwebtoken";
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const createJwtToken = (payload: object): Promise<string> => {
+export const createAccessToken = (payload: object): Promise<string> => {
     return new Promise((resolve, reject) => {
         const { JWT_PRIVATE_KEY } = process.env;
         if (!JWT_PRIVATE_KEY) {
             throw new Error("JWT_PRIVATE_KEY is not defined in environment variables");
         }
-        jwt.sign({ payload }, JWT_PRIVATE_KEY, { expiresIn: "1h" }, (err, token) => {
+        jwt.sign({ payload }, JWT_PRIVATE_KEY, { expiresIn: "10s" }, (err, token) => {
             if (err) {
                 reject(err);
             }
@@ -24,15 +24,15 @@ export const createRefreshToken = (payload: object): Promise<string> => {
             throw new Error("JWT_REFRESH_KEY is not defined in environment variables");
         }
         jwt.sign({ payload }, JWT_REFRESH_KEY, { expiresIn: "7d" }, (err, token) => {
-            if (err) {
-                reject(err);
+            if(err) {
+                return reject(err);
             }
             resolve(token as string);
         });
     });
 };
 
-export const convertJwtToken = (token: string): Promise<object> => {
+export const convertAccessToken = (token: string): Promise<object> => {
     return new Promise((resolve, reject) => {
         const { JWT_PRIVATE_KEY } = process.env;
         if (!JWT_PRIVATE_KEY) {
@@ -40,9 +40,20 @@ export const convertJwtToken = (token: string): Promise<object> => {
         }
         jwt.verify(token, JWT_PRIVATE_KEY, (err, decoded) => {
             if (err) {
-                if (err.name === "TokenExpiredError") {
-                    return reject(new Error("Token has expired"));
-                }
+                return reject(err);
+            }
+            resolve(decoded as object);
+        });
+    });
+};
+export const convertRefreshToken = (token: string): Promise<object> => {
+    return new Promise((resolve, reject) => {
+        const { JWT_REFRESH_KEY } = process.env;
+        if (!JWT_REFRESH_KEY) {
+            throw new Error("JWT_PRIVATE_KEY is not defined in environment variables");
+        }
+        jwt.verify(token, JWT_REFRESH_KEY, (err, decoded) => {
+            if (err) {
                 return reject(err);
             }
             resolve(decoded as object);
