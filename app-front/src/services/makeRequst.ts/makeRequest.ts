f93e5d3053f94529db85219c7c3ba91b959c751b
Adam Myślicki <amyslicki2002@gmail.com>
2025-01-12 15:59:15 +0100

    [scrum-22] create makeRequest factory types

--- app-front/src/services/makeRequst.ts/makeRequest.ts
+++ app-front/src/services/makeRequst.ts/makeRequest.ts
@@ -1,19 +1,24 @@
 import { HttpMethod, GETRequestEnum, POSTRequestEnum, DELETERequestEnum, GETRequestProps, POSTRequestProps, DELETERequestProps, RequestOptions  } from "./makeRequest.types"
+import {RequestResponse} from "./response.types"
+type RequestProps<T> = T extends GETRequestEnum
+  ? GETRequestProps[T]
+  : T extends POSTRequestEnum
+  ? POSTRequestProps[T]
+  : T extends DELETERequestEnum
+  ? DELETERequestProps[T]
+  : never;
 
-function requestFactory(method: HttpMethod, type: GETRequestEnum | POSTRequestEnum | DELETERequestEnum, props: GETRequestProps | POSTRequestProps | DELETERequestProps): RequestOptions {
+function requestFactory<T extends GETRequestEnum | POSTRequestEnum | DELETERequestEnum>(
+  method: HttpMethod,
+  type: T,
+  props: RequestProps<T>
+) {
   const baseUrl = import.meta.env.VITE_API_BASE_URL
   let url = `${baseUrl}/${type}`
   let body: string | undefined
 
-  if (method === 'POST' && props) {
+  if(props) {
     body = JSON.stringify(props)
-  }
-
-  if (method === 'GET' && typeof props === 'object') {
-  }
-
-  if(method === 'POST') {
-
   }
 
   return {
@@ -25,18 +30,24 @@ function requestFactory(method: HttpMethod, type: GETRequestEnum | POSTRequestEn
     },
     body,
     url,
-  }
+  };
 }
 
-async function makeRequest<T>(method: HttpMethod, type: GETRequestEnum | POSTRequestEnum | DELETERequestEnum, props: GETRequestProps | POSTRequestProps | DELETERequestProps): Promise<T> {
-  const requestOptions = requestFactory(method, type, props)
-
-  const response = await fetch(requestOptions.url, requestOptions)
+async function makeRequest<T extends GETRequestEnum | POSTRequestEnum | DELETERequestEnum>(
+  method: HttpMethod,
+  type: T,
+  props: RequestProps<T>
+): Promise<RequestResponse<T>> {
+  console.log(props);
+  const requestOptions = requestFactory(method, type, props);
+  const response = await fetch(requestOptions.url, requestOptions);
   if (!response.ok) {
-    throw new Error(`error status: ${response.status}`)
+    throw new Error(`error status: ${response.status}`);
   }
 
-  return await response.json()
+  return await response.json();
 }
 
-export { makeRequest }
+export { makeRequest};
+
+
--- app-front/src/views/main/LoginView/LoginView.vue
+++ app-front/src/views/main/LoginView/LoginView.vue
@@ -1,12 +1,33 @@
 <template>
   <div>
     <Navigation />
-    loginview
+    <input type="text" placeholder="Username/email" v-model="username" />
+    <input type="password" placeholder="Password" v-model="password" />
+    <button @click="login">Login</button>
   </div>
 </template>
 
 <script setup lang="ts">
 import Navigation from '@FlashCardsUi/Navigation/Navigation.vue'
+import { LoginProps, POSTRequestEnum } from '../../../services/makeRequst.ts/makeRequest.types'
+import { ref } from 'vue'
+import { makeRequest } from '../../../services/makeRequst.ts/makeRequest'
+const username = ref('')
+const password = ref('')
+
+function login() {
+  const data:LoginProps = {
+    usernameOrEmail: username.value,
+    password: password.value,
+  }
+  makeRequest('POST', POSTRequestEnum.LOGIN, data)
+    .then((response) => {
+      console.log(response)
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+}
 </script>
 
 <style scoped>
