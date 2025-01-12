1aa072a11a7ec8363448348e7a2c23ca4b4d17ef
Dominik Obuch <dominik.obuch@wp.pl>
2025-01-12 12:07:55 +0100

    [SCRUM-19] Added logout route

--- app-backend/api/app.ts
+++ app-backend/api/app.ts
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import { fileURLToPath } from 'url';
 import registerRoute from './routes/registerRoute.js';
 import loginRoute from './routes/loginRoute.js';
+import logoutRoute from './routes/logoutRoute.js'
 import flashcardsRoute from './routes/flashcardsRoute.js';
 import refreshTokenRoute from './routes/refreshTokenRoute.js';
 import cookieParser from 'cookie-parser';
@@ -17,9 +18,9 @@ const port = 3000;
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-
 app.use('/register', registerRoute);
 app.use('/login', loginRoute);
+app.use('/logout', logoutRoute);
 app.use('/flashcards', flashcardsRoute);
 app.use('/refresh-token', refreshTokenRoute);
 
