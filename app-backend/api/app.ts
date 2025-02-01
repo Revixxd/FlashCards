d35fcfc1894b880a31863befbac97442a579a0bc
Dominik Obuch <dominik.obuch@wp.pl>
2025-02-01 14:44:41 +0100

    [SCRUM-30] Add cors for front-end

--- app-backend/api/app.ts
+++ app-backend/api/app.ts
@@ -17,7 +17,10 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use('/register', registerRoute);
