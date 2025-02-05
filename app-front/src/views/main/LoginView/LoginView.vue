62b369be4f9a4562d85c6b791d28f579c94751be
KrzysztofDukiel <krdukiel@interia.pl>
2025-02-05 19:49:16 +0100

    [Scrum 9] styling register/login view with scss

--- app-front/src/views/main/LoginView/LoginView.vue
+++ app-front/src/views/main/LoginView/LoginView.vue
@@ -2,30 +2,24 @@
 
   <div class="auth">
     <form @submit.prevent="formSubmit" class="auth__form">
-      <h1 class="auth__title">{{ isLogin ? 'Login to FlashCards' : 'Register to FlashCards' }}</h1>
+      <h1 class="auth__title">Login to FlashCards</h1>
 
       <p class="auth__label">Enter your username</p>
       <input type="text" v-model="formData.usernameOrEmail" name="username" class="auth__input" required />
 
-      <p v-if="!isLogin" class="auth__label">Enter your email</p>
-      <input v-if="!isLogin" type="email" v-model="formData.usernameOrEmail" name="username" class="auth__input" required />
-
       <p class="auth__label">Enter your password</p>
       <input type="password" v-model="formData.password" name="password" class="auth__input" required />
-      
-      <p v-if="!isLogin" class="auth__label">Confrim your password</p>
-      <input v-if="!isLogin" type="password"  v-model="confirmedPassword" name="email" class="auth__input" required />
 
       <button type="submit" class="auth__button">
-        {{ isLogin ? 'Login' : 'Register' }}
+        Login
       </button>
 
       <p v-if="requestError" class="error-text">{{ requestError.message }}</p>
 
       <p class="toggle__text">
-        {{ isLogin ? "Don't have an account?" : "Already have an account?" }}
-        <button type="button" @click="toggleForm" class="toggle__button">
-          {{ isLogin ? 'Register' : 'Login' }}
+        Don't have an account?
+        <button type="button" @click="goToRegister" class="toggle__button">
+          Register
         </button>
       </p>
     </form>
@@ -36,110 +30,115 @@
 import { ref } from 'vue'
 import useLogin from '../../../utils/useLogin/useLogin'
 import type { LoginProps } from '../../../services/makeRequest/makeRequest.types'
+import { useRouter } from 'vue-router';
 
 const formData = ref<LoginProps>({
   usernameOrEmail: '',
   password: '',
 })
 const { login, requestError } = useLogin()
-
-const isLogin = ref(true)
-const confirmedPassword = ref('')
-function toggleForm(){
-  isLogin.value = !isLogin.value
-}
-
+const router = useRouter();
 function formSubmit() {
-  if(isLogin.value) {
     login(formData.value)
-  } else {
-    if(confirmedPassword === formData.value.password) {
-
-    } else {
-
-    }
-  }
 }
+const goToRegister = () => {
+  router.push('/register');
+};
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+@import "@src/styles/variables.scss";
+
 .auth {
   display: flex;
   justify-content: center;
   align-items: center;
   min-height: 100vh;
+  background: $background-color;
+
+  &__form {
+    width: 400px;
+    padding: 20px;
+    background: $secondary-background-color;
+    border-radius: 10px;
+    text-align: center;
+    box-shadow: 0 0 10px rgba($text-color, 0.1);
+  }
+
+  &__title {
+    color: $text-color;
+    font-size: 24px;
+    margin-bottom: 15px;
+  }
+
+  &__label {
+    color: $secondary-color;
+    margin: 5px 0;
+    text-align: left;
+    font-weight: bold;
+  }
+
+  &__input {
+    width: 90%;
+    padding: 10px;
+    margin-bottom: 10px;
+    border: 1px solid $secondary-color;
+    background-color: lighten($primary-color, 10%);
+    color: $text-color;
+    border-radius: 5px;
+    outline: none;
+    transition: border-color 0.3s;
+
+    &::placeholder {
+      color: darken($secondary-color, 10%);
+    }
+
+    &:focus {
+      border-color: $color-light;
+    }
+  }
+
+  &__button {
+    width: 50%;
+    padding: 10px;
+    background-color: $color-light;
+    color: $text-color;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    font-size: 16px;
+    transition: background 0.3s;
+
+    &:hover {
+      background-color: darken($color-light, 10%);
+    }
+  }
 }
 
-.auth__form {
-  width: 400px;
-  padding: 20px;
-  background: #1E1E30;
-  border-radius: 10px;
-  text-align: center;
-  box-shadow: 0 0 10px rgba(255, 255, 255, 0.1);
-}
+.toggle {
+  &__text {
+    margin-top: 10px;
+    color: lighten($secondary-color, 20%);
+  }
 
-.auth__title {
-  color: white;
-  font-size: 24px;
-  margin-bottom: 15px;
-}
+  &__button {
+    background: none;
+    border: none;
+    color: $color-light;
+    cursor: pointer;
+    transition: color 0.3s;
 
-.auth__label {
-  color: #aaa;
-  margin: 5px 0;
-  text-align: left;
-}
-
-.auth__input {
-  width: 90%;
-  padding: 10px;
-  margin-bottom: 10px;
-  border: 1px solid #555;
-  background-color: #2B2B40;
-  color: white;
-  border-radius: 5px;
-  outline: none;
-}
-
-.auth__input::placeholder {
-  color: #888;
-}
-
-.auth__button {
-  width: 30%;
-  padding: 10px;
-  background-color: #3b82f6;
-  color: white;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  font-size: 16px;
-}
-
-.auth__button:hover {
-  background-color: #2563eb;
-}
-
-.toggle__text {
-  margin-top: 10px;
-  color: #bbb;
-}
-
-.toggle__button {
-  background: none;
-  border: none;
-  color: #3b82f6;
-  cursor: pointer;
-}
-
-.toggle__button:hover {
-  text-decoration: underline;
+    &:hover {
+      text-decoration: underline;
+      color: lighten($color-light, 20%);
+    }
+  }
 }
 
 .error-text {
-  color: red;
+  color: $accent-color;
   font-size: 14px;
   margin-top: 10px;
+  font-weight: bold;
 }
 </style>
--- app-front/src/views/main/RegisterView/RegisterView.vue
+++ app-front/src/views/main/RegisterView/RegisterView.vue
@@ -1,14 +1,37 @@
 <template>
   <div>
-    <input v-model="formData.username" type="text" placeholder="Username">
-    <input v-model="formData.password" type="password" placeholder="Password">
-    <input v-model="formData.email" type="email" placeholder="Email">
-    <button @click="formSubmit">
-      Register
-    </button>
-    <p v-if="requestError">
+    <div class="auth">
+    <form @submit.prevent="formSubmit" class="auth__form">
+      <h1 >Register to flashcards</h1>
+
+      <p class="auth__label">Enter your username</p>
+      <input type="text" v-model="formData.username" name="username" class="auth__input" required />
+
+      <p class="auth__label">Enter your email</p>
+      <input  type="email" v-model="formData.email" name="username" class="auth__input" required />
+
+      <p class="auth__label">Enter your password</p>
+      <input type="password" v-model="formData.password" name="password" class="auth__input" required />
+      
+      <p  class="auth__label">Confrim your password</p>
+      <input  type="password"  v-model="confirmedPassword" name="email" class="auth__input" required />
+
+      <button type="submit" class="auth__button">
+        Register
+      </button>
+
+      <p v-if="requestError">
       {{ requestError.message }}
-    </p>
+      </p>
+
+      <p class="toggle__text">
+        Already have an account?
+        <button type="button" @click="goToLogin" class="toggle__button">
+         Login
+        </button>
+      </p>
+    </form>
+  </div>
   </div>
 </template>
 
@@ -16,20 +39,122 @@
 import type { RegisterProps } from '../../../services/makeRequest/makeRequest.types'
 import { ref } from 'vue'
 import useRegister from '../../../utils/useRegister/useRegister'
+import { useRouter } from 'vue-router';
 
+const router = useRouter();
 const formData = ref<RegisterProps>({
   username: '',
   email: '',
   password: '',
 })
-
+const confirmedPassword = ref('')
 const { register, requestError } = useRegister()
 
 function formSubmit() {
-  register(formData.value)
+  if(confirmedPassword == formData.value.password) {
+    register(formData.value)
+  } else {
+
+  }
 }
+const goToLogin = () => {
+  router.push('/login');
+};
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+@import "@src/styles/variables.scss";
 
+.auth {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background: $background-color;
+
+  &__form {
+    width: 400px;
+    padding: 20px;
+    background: $secondary-background-color;
+    border-radius: 10px;
+    text-align: center;
+    box-shadow: 0 0 10px rgba($text-color, 0.1);
+  }
+
+  &__title {
+    color: $text-color;
+    font-size: 24px;
+    margin-bottom: 15px;
+  }
+
+  &__label {
+    color: $secondary-color;
+    margin: 5px 0;
+    text-align: left;
+    font-weight: bold;
+  }
+
+  &__input {
+    width: 90%;
+    padding: 10px;
+    margin-bottom: 10px;
+    border: 1px solid $secondary-color;
+    background-color: lighten($primary-color, 10%);
+    color: $text-color;
+    border-radius: 5px;
+    outline: none;
+    transition: border-color 0.3s;
+
+    &::placeholder {
+      color: darken($secondary-color, 10%);
+    }
+
+    &:focus {
+      border-color: $color-light;
+    }
+  }
+
+  &__button {
+    width: 50%;
+    padding: 10px;
+    background-color: $color-light;
+    color: $text-color;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    font-size: 16px;
+    transition: background 0.3s;
+
+    &:hover {
+      background-color: darken($color-light, 10%);
+    }
+  }
+}
+
+.toggle {
+  &__text {
+    margin-top: 10px;
+    color: lighten($secondary-color, 20%);
+  }
+
+  &__button {
+    background: none;
+    border: none;
+    color: $color-light;
+    cursor: pointer;
+    transition: color 0.3s;
+
+    &:hover {
+      text-decoration: underline;
+      color: lighten($color-light, 20%);
+    }
+  }
+}
+
+.error-text {
+  color: $accent-color;
+  font-size: 14px;
+  margin-top: 10px;
+  font-weight: bold;
+}
 </style>
